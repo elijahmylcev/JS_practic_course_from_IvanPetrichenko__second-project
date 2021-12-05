d783b2aecbce2169471167d3d9af671df5a72b1e
Elijah <elijahmylcev@gmail.com>
2021-12-05 03:17:28 +0300

    Show more items - first method [display property]

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import checkTextInputs from './modules/checkTextInputs';
 import forms from './modules/forms';
 import mask from './modules/mask';
 import modals from './modules/modals';
+import showMoreStyles from './modules/showMoreStyles';
 import sliders from './modules/sliders';
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -16,4 +17,5 @@ window.addEventListener('DOMContentLoaded', () => {
   mask('[name="phone"]');
   checkTextInputs('[name="name"]');
   checkTextInputs('[name="message"]');
+  showMoreStyles('.button-styles', '.styles-2');
 });
